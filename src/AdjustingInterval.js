3a0856d4836a5c867d31ea317cd44c1248fcc2eb
Jussi Vatjus-Anttila <jussi.vatjus-anttila@arm.com>
2018-12-09 14:59:44 +0200

    wip

--- src/AdjustingInterval.js
+++ src/AdjustingInterval.js
@@ -10,25 +10,24 @@ class AdjustingInterval extends EventEmitter {
    *                             for each interval
    * @param {Number}      interval  Interval speed (in milliseconds)
    */
-  constructor(handler, interval = 1000) {
-
+  constructor(handler, interval = 1000, ...args) {
     super();
     this._expected = undefined;
-    this._timeout = undefined;
+    this._timeout = false;
     this.interval = interval;
     this._workFunc = handler;
+    this._args = args;
     this._step = this._step.bind(this);
     // TODO: better estimate for next tick..
     //this._windowSize = 10;
     //this._driftHistory = new Uint32Array(this._windowSize).fill(0)
     //this._index = 0;
-    this.start();
   }
   start() {
     /**
      * Start timer
      */
-    this.stop();
+    clearTimeout(this._timeout);
     this._expected = Date.now() + this.interval;
     this._timeout = setTimeout(this._step, this.interval);
   }
@@ -38,18 +37,20 @@ class AdjustingInterval extends EventEmitter {
      * Stop timer
      */
     clearTimeout(this._timeout);
+    this._timeout = false;
   }
-
   _step() {
     const drift = Date.now() - this._expected;
     if (drift > this.interval) {
       const error = new Error(`Drift was more than interval: ${drift} > ${interval}`);
       this.emit('error', error);
     }
-    this._workFunc(drift);
-    this._expected += this.interval;
-    const after = Math.max(0, this.interval - drift);
-    this._timeout = setTimeout(this._step, after);
+    this._workFunc(...this._args);
+    if (this._timeout !== false) {
+      this._expected += this.interval;
+      const after = Math.max(0, this.interval - drift);
+      this._timeout = setTimeout(this._step, after);
+    }
   }
 
   /**
@@ -58,8 +59,10 @@ class AdjustingInterval extends EventEmitter {
    * @param {Number} interval
    * @return {AdjustingInterval}
    */
-  static setInterval(handler, interval) {
-    return new AdjustingInterval(handler, interval);
+  static setInterval(handler, interval, ...args) {
+    const timer = new AdjustingInterval(handler, interval, ...args);
+    timer.start();
+    return timer;
   }
   /**
    * Native clearInterval() replacement function
