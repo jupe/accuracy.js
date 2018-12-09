1b288246ec27215812598ff32d19a30e7908bc48
Jussi Vatjus-Anttila <jussi.vatjus-anttila@arm.com>
2018-12-09 15:06:41 +0200

    test

---
+++ test/FrequencyCounter.js
@@ -0,0 +1,31 @@
+const {expect} = require('chai');
+
+const {AdjustingInterval, FrequencyCounter} = require('..');
+const {setInterval, clearInterval} = AdjustingInterval;
+
+describe('FrequencyCounter', function () {
+
+  describe('frequences', function () {
+    let counter;
+    beforeEach(function () {
+      counter = new FrequencyCounter();
+    });
+    const tests = [
+      {frequency: 100, n: 1, interval: 10, duration: 1000},
+      {frequency: 1000, n: 10, interval: 10, duration: 2000}
+    ];
+    tests.forEach(({frequency, n, interval, duration}) => {
+      it(`${frequency}Hz`, function (done) {
+        this.timeout(duration*2);
+        let timer = setInterval(() => counter.inc(n), interval);
+        setTimeout(() => {
+          const freq = counter.freq();
+          clearInterval(timer);
+          const limits = frequency * 0.1;
+          expect(freq).to.be.within(frequency-limits, frequency+limits);
+          done();
+        }, duration);
+      });
+    });
+  });
+});
